49bf5035bfb97e68f9e9732215280bd2369d2195
aorebus <orebusalex@gmail.com>
2020-05-13 13:40:14 +0300

    Square + Rectangle

---
+++ sandbox/src/main/java/ru/stqa/orab1/sandbox/Square.java
@@ -0,0 +1,14 @@
+package ru.stqa.orab1.sandbox;
+
+public class Square {
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+  public double area() {
+    return this.l  *  this.l;
+
+
+  }
+}
